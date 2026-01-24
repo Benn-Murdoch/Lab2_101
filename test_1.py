7b2ebd2772434b10f2d08b95bec5df7076840e15
Bennjamen Murdoch <bsmurdoc@calpoly.edu>
2026-01-24 00:31:19 +0000

    day 1 done

--- test_1.py
+++ test_1.py
@@ -5,29 +5,29 @@ class TestLab2(unittest.TestCase):
 
     # --- Tests for get_first_alphabetically ---
     def test_get_first_1(self):
-        pass
+        self.assertEqual(get_first_alphabetically("a","b"),"a")
 
 
     def test_get_first_2(self):
-        pass
+        self.assertEqual(get_first_alphabetically("b","a"),"a")
         
 
 
     def test_get_first_3(self):
-        pass
-
-
+        self.assertEqual(get_first_alphabetically("A","Z"),"A")
+   
+   
     # --- Tests for calculate_triangle_area ---
     def test_area_1(self):
-        pass
+        self.assertEqual(calculate_triangle_area(2,3),3.0)
 
 
     def test_area_2(self):
-        pass
+        self.assertEqual(calculate_triangle_area(2,9),9.0)
 
 
     def test_area_3(self):
-        pass
+        self.assertEqual(calculate_triangle_area(8,8),32.0)
 
 
 if __name__ == '__main__':
