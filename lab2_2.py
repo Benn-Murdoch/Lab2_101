23e44974f239221defd9327c0bcadfb5910017c8
Bennjamen Murdoch <bsmurdoc@calpoly.edu>
2026-01-27 00:43:33 +0000

    day 2 done

--- lab2_2.py
+++ lab2_2.py
@@ -1,12 +1,24 @@
 def reverse_list(data):
-    """Returns a new list with elements in reverse order."""
-    pass
+   return data[::-1]
+   # """Returns a new list with elements in reverse order."""
+  
 
 def get_ends(data):
-    """Returns a new list containing only the first and last elements."""
-    pass
+    return [data[0],data[-1]]
+    #"""Returns a new list containing only the first and last elements."""
+    
 
 def get_middle(data):
-    """Returns the middle element. For even lengths, rounds down to the lower index."""
 
-    pass
+    list_len = len(data)
+
+    if list_len%2 == 0:
+        middle_value = int((list_len//2)-1) 
+        return data[middle_value]
+    else:
+        middle_value = int((list_len-1)/2)
+        return data[middle_value]
+
+    #"""Returns the middle element. For even lengths, rounds down to the lower index."""
+
+print(get_middle(["a", "b", "c", "d", "e" "f"]))
