9086b49592c596de04a229b9f3bd17e5ab2e9ae3
Bennjamen Murdoch <bsmurdoc@calpoly.edu>
2026-01-29 00:38:37 +0000

    day 3 done

--- lab2_3.py
+++ lab2_3.py
@@ -1,11 +1,21 @@
 def alphabetize_two(word1, word2):
-    """Returns a single string of both words in alphabetical order with a space between."""
-    pass
+    if word1 > word2:
+        return word2 + " " + word1
+    else:
+        return word1 + " " + word2
+    # """Returns a single string of both words in alphabetical order with a space between."""
+    
 
 def repeat_string(text, n):
-    """Returns the string text repeated n times."""
-    pass
+    return text * n
+    # """Returns the string text repeated n times."""
 
 def swap_ends(text):
-    """Swaps the first and last character of a string."""
-    pass
+    if len(text) > 2: 
+        return text[-1] + text[1:-1] + text[0]
+    else: 
+        return text
+    # """Swaps the first and last character of a string."""
+      
+
+print(swap_ends("beans"))
