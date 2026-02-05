c29d4b750aeb3bd3e793910573c416f21de49b56
Bennjamen Murdoch <bsmurdoc@calpoly.edu>
2026-02-05 02:41:15 +0000

    Lab 2 Complete

--- lab2_6.py
+++ lab2_6.py
@@ -1,11 +1,17 @@
 def multiplication_table():
     """Returns a list of products for a 12x12 table (1x1, 1x2... 12x12)."""
-    pass
+    table = []
+    for i in range(1,13):
+        for j in range(1,13):
+            table.append(i*j)
+    return table
+
 
 def get_multiples_of_three(numbers):
     """Uses a list comprehension to return numbers > 0 and divisible by 3."""
-    pass
+    return [num for num in numbers if num>0 and num%3==0]
+
 
 def merge_lists(list1, list2):
     """Uses zip to merge two lists into a list of tuples."""
-    pass
+    return list(zip(list1 , list2))
